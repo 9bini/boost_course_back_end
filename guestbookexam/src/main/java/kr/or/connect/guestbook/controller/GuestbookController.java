414ccd483c962252a6091aa49345a2b76d73c331
9bini <jinhoa52@gmail.com>
2021-05-07 09:58:44 +0900

    쿠키를 이용한 상태정보 유지하기-1

--- guestbookexam/src/main/java/kr/or/connect/guestbook/controller/GuestbookController.java
+++ guestbookexam/src/main/java/kr/or/connect/guestbook/controller/GuestbookController.java
@@ -3,7 +3,9 @@ package kr.or.connect.guestbook.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -22,7 +24,36 @@ public class GuestbookController {
 	GuestbookService guestbookService;
 
 	@GetMapping(path = "/list")
-	public String list(@RequestParam(name = "start", required = false, defaultValue = "0") int start, ModelMap model) {
+	public String list(@RequestParam(name = "start", required = false, defaultValue = "0") int start, ModelMap model,
+			HttpServletRequest request, HttpServletResponse response) {
+
+		String value = null;
+		boolean find = false;
+		Cookie[] cookies = request.getCookies();
+		if (cookies != null) {
+			for (Cookie cookie : cookies) {
+				if ("count".equals(cookie.getName())) {
+					value = cookie.getValue();
+					find = true;
+				}
+			}
+		}
+
+		if (!find) {
+			value = "1";
+		} else {
+			try {
+				int i = Integer.valueOf(value);
+				value = Integer.toString(i + 1);
+			} catch (Exception e) {
+				value = "1";
+			}
+		}
+		
+		Cookie cookie = new Cookie("count", value);
+		cookie.setMaxAge(60 * 60 * 24 * 365);
+		cookie.setPath("/"); // 적용 범위(경로)를 지정하는 것은 상상도 못했다. 
+		response.addCookie(cookie);
 
 		List<Guestbook> list = guestbookService.getGuestbooks(start);
 
@@ -39,7 +70,7 @@ public class GuestbookController {
 		model.addAttribute("list", list);
 		model.addAttribute("count", count);
 		model.addAttribute("pageStartList", pageStartList);
-
+		model.addAttribute("cookieCount", value);
 		return "list";
 	}
 
